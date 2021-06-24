27fecfc7c43fc60c924a91b3266ebcac6013000b
Jamison Hyman <jamison.hyman@gmail.com>
2021-06-23 19:08:15 -0700

    formatting

--- src/server/views/components/login/__tests__/index.test.js
+++ src/server/views/components/login/__tests__/index.test.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import { render, waitFor, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Login from '../'
 
 test('loads and displays login page', async () => {
-    render(<Login />)
+  render(<Login />)
 
-  
-    await waitFor(() => screen.getByRole('heading'))
-  
-    expect(screen.getByRole('heading')).toHaveTextContent('Welcome to TutorRow')
-  })
+  await waitFor(() => screen.getByRole('heading'))
+
+  expect(screen.getByRole('heading')).toHaveTextContent('Welcome to TutorRow')
+})
